d40c5c0db5f8b64681905535df7cff960e72fb0f
Tim D. Smith <git@tim-smith.us>
2012-12-16 13:31:53 -0800

    Added reference gene stability ranking according to Vandesompele 2002.

--- expression.py
+++ expression.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 from math import isnan, log
+from numpy import mean, std
 from types import *
+
+log2 = lambda x: log(x)/log(2)
 
 def average_cq(seq, efficiency=1.0):
     denominator = sum( [pow(2.0*efficiency, -Ci) for Ci in seq] )
     return log(len(seq)/denominator)/log(2.0*efficiency)
 
 def expression(sample_list, ref_gene, ref_sample):
+    # out, ignored_targets, ignored_samples = expression(sample_list, ref_gene, ref_sample)
     # sample_list has form [['target1', 'sample1', Cq1], ...]
     # replicates for experimental genes are considered independently
     # replicates for reference genes are averaged for referencing
@@ -64,3 +68,38 @@ def expression(sample_list, ref_gene, ref_sample):
                 out.setdefault(target, {}).setdefault(sample, []).append(rel)
 
     return (out, ignored_targets, ignored_samples)
+
+def rank_genes(sample_list, ref_genes, ref_sample):
+    # applies the Vandesompele et al. (2002) method to rank reference genes
+    # in order of stability.
+    # doi:10.1186/gb-2002-3-7-research0034
+
+    ref_genes = set(ref_genes)
+
+    # first, normalize all samples by each of the reference genes
+    table = {}
+    available_samples = set([sample[1] for sample in sample_list])
+    for ref_gene in ref_genes:
+        table[ref_gene], ignored_targets, ignored_samples = expression(sample_list, ref_gene, ref_sample)
+        if set(ignored_targets).intersection(ref_genes):
+            raise ValueError("Bad reference sample. One or more reference gene values are not available.")
+        available_samples -= set(ignored_samples)
+
+    worst = []
+    while len(ref_genes) - len(worst) > 2:
+        # calculate M for each gene
+        M = []
+        for test_gene in ref_genes:
+            if test_gene in worst: continue
+            Vs = []
+            for ref_gene in ref_genes:
+                if ref_gene == test_gene: continue
+                if ref_gene in worst: continue
+                # compute A_(ref_gene, test_gene)
+                A = [log2(mean(table[ref_gene][test_gene][sample])) for sample in available_samples]
+                Vs.append(std(A))
+            M.append( (sum(Vs) / (len(ref_genes) - len(worst) - 1), test_gene) )
+        worst.append(max(M)[1])
+    best = ref_genes - set(worst) # whatever's left over
+    worst.reverse()
+    return list(best) + worst
--- test_expression.py
+++ test_expression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
-from expression import expression, average_cq
+from expression import expression, average_cq, rank_genes
 from types import *
 
 mean = lambda seq: float(sum(seq))/len(seq)
@@ -56,6 +56,31 @@ class TestAverage(unittest.TestCase):
     def test_averaging(self):
         self.assertAlmostEqual(average_cq((3.0,5.0)), 3.678071905112638)
         self.assertAlmostEqual(average_cq((3.0,4.0,5.0)), 3.7776075786635523)
+
+class TestRankGenes(unittest.TestCase):
+    def test_ranking(self):
+        with open('vandesompele-2002-cq.txt') as f:
+            buf = f.readlines()
+        gene_names = buf[0][:-1].split('\t')[2:]
+        # slicing to cut off newlines on each line and skip header row
+        # yields ['Tissue', 'Sample', Cq0, Cq1, Cq2...]
+        buf = [line[:-1].split('\t') for line in buf[1:] if len(line) > 2]
+        d = {}
+        for line in buf:
+            for (index, name) in enumerate(gene_names):
+                d.setdefault(line[0], []).append([name, line[1], float(line[2+index])])
+        # e.g. d['Fib'] = [['ACTB', 'Fib1', 12.34], ...]
+        key = {'Neuroblastoma': ['HPRT1', 'GAPD', 'SDHA', 'UBC', 'HMBS', 'YWHAZ', 'TBP', 'ACTB', 'RPL13A', 'B2M'],
+               'Fib': ['HPRT1', 'GAPD', 'YWHAZ', 'UBC', 'ACTB', 'TBP', 'SDHA', 'RPL13A', 'B2M', 'HMBS'],
+               'Leukocyte': ['UBC', 'YWHAZ', 'B2M', 'GAPD', 'RPL13A', 'TBP', 'SDHA', 'HPRT1', 'HMBS', 'ACTB'],
+               'BM': ['UBC', 'RPL13A', 'YWHAZ', 'HPRT1', 'GAPD', 'SDHA', 'TBP', 'HMBS', 'B2M', 'ACTB'],
+               'Pool': ['SDHA', 'GAPD', 'HMBS', 'HPRT1', 'TBP', 'UBC', 'RPL13A', 'YWHAZ', 'ACTB', 'B2M']}
+        for tissue in d:
+            ranked = rank_genes(d[tissue], gene_names, d[tissue][0][1])
+            # no preference between the two best genes
+            self.assertIn(ranked[0], key[tissue][:2])
+            self.assertIn(ranked[1], key[tissue][:2])
+            self.assertEqual(ranked[2:], key[tissue][2:])
 
 if __name__ == '__main__':
     unittest.main()
