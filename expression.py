5e0550887c4fda89b7f525dc4c47c9d990e66b51
Tim D. Smith <git@tim-smith.us>
2014-02-14 22:12:56 -0800

    Remove old functions with new frame equivalents

--- expression.py
+++ expression.py
@@ -18,12 +18,14 @@ def average_cq(seq, efficiency=1.0):
     denominator = sum( [pow(2.0*efficiency, -Ci) for Ci in seq] )
     return log(len(seq)/denominator)/log(2.0*efficiency)
 
+"""
 def make_sample_dict(sample_frame):
     d = {}
     g = sample_frame.groupby(['Target', 'Sample'])
     for (target, sample), h in g:
         d.setdefault(target, {})[sample] = list(h['Cq'])
     return d
+"""
 
 def validate_sample_frame(sample_frame):
     if not isinstance(sample_frame, pd.core.frame.DataFrame):
@@ -34,19 +36,6 @@ def validate_sample_frame(sample_frame):
     if sample_frame['Cq'].dtype.kind != 'f':
         raise ValueError("Expected Cq column to have float type; has type {} instead".format(str(sample_frame['Cq'].dtype)))
     return True
-
-def censor_background(d):
-    # censor anything too close to any NTCs we have and remove NTCs from d
-    # operates on d in place
-    margin = log2(10)
-    for target in d:
-        if not ('NTC' in d[target]): continue
-        too_close = min(d[target]['NTC']) - margin
-        for sample in d[target].keys():
-            if sample == 'NTC': continue
-            d[target][sample] = [i for i in d[target][sample] if i < too_close]
-            if len(d[target][sample]) == 0: del d[target][sample]
-        del d[target]['NTC']
 
 def censor_frame_background(sdf, ntc_samples, margin):
     ntcs = sdf.loc[ sdf['Sample'].apply(lambda x: x in ntc_samples), ]
@@ -60,54 +49,6 @@ def censor_frame_background(sdf, ntc_samples, margin):
     # so we test for the "wrong" condition and invert the result
     censored = sdf.loc[ ~(sdf['Cq'] > (min_ntcs.loc[sdf['Target']] - margin)) ]
     return censored
-
-def censor_targets_missing_refsample(d, ref_sample):
-    # all genes must have a reference sample
-    ignored_targets = []
-    for target in d.keys():
-        if ref_sample not in d[target]:
-            ignored_targets.append(target)
-            del d[target]
-    return ignored_targets
-
-def expression(sample_list, ref_gene, ref_sample):
-    # out, ignored_targets, ignored_samples = expression(sample_list, ref_gene, ref_sample)
-    # sample_list has form [['target1', 'sample1', Cq1], ...]
-    # replicates for experimental genes are considered independently
-    # replicates for reference genes are averaged for referencing
-    # returns (sample_dictionary, [ignored genes], [ignored samples])
-    # sample_dictionary has the form s_d[target][sample] = [rel0, rel1] where
-    # each rel_n is from a replicate in the input list and is relative to the
-    # reference sample
-
-    d = make_sample_dict(sample_list)
-    censor_background(d)
-    ignored_targets = censor_targets_missing_refsample(d, ref_sample)
-
-    # reference genes need to be defined for each sample. ID them first
-    ignored_samples = []
-    for target in d:
-        for sample in d[target]:
-            if not(sample in d[ref_gene]):
-                ignored_samples.append(sample)
-
-    # and now clean up
-    for target in d:
-        for ignoreme in ignored_samples:
-            if ignoreme in d[target]:
-                del d[target][ignoreme]
-    
-    out = {}
-    # perform 2**delta-delta Cq expression comparison
-    for target in d:
-        for sample in d[target]:
-            for cq in d[target][sample]:
-                delta_ref = average_cq(d[ref_gene][sample]) - average_cq(d[ref_gene][ref_sample])
-                delta_target = cq - average_cq(d[target][ref_sample])
-                rel = pow(2, delta_ref - delta_target)
-                out.setdefault(target, {}).setdefault(sample, []).append(rel)
-
-    return (out, ignored_targets, ignored_samples)
 
 def expression_frame(sdf, ref_target, ref_sample, ntc_samples=['NTC'], ntc_margin=log2(10)):
     # It might be more correct to replace asarray calls (to discard indexes)
@@ -157,59 +98,11 @@ def expression_nf(sample_list, nfs, ref_sample):
 
     return (out, ignored_targets, ignored_samples)
 
-
-def collect_expression(sample_frame, ref_genes, ref_sample):
-    # normalize all samples by each of the reference genes
-    table = {}
-    if not (type(ref_genes) is set): ref_genes = set(ref_genes)
-    available_samples = set(sample_frame['Sample']) - {'NTC'}
-    for ref_gene in ref_genes:
-        table[ref_gene], ignored_targets, ignored_samples = expression(sample_frame, ref_gene, ref_sample)
-        if set(ignored_targets).intersection(ref_genes):
-            raise ValueError("Bad reference sample. One or more reference gene values are not available.")
-        available_samples -= set(ignored_samples)
-    return table, available_samples
-
 def collect_expression_frame(sample_frame, ref_targets, ref_sample):
     by_gene = {'Sample': sample_frame['Sample'], 'Target': sample_frame['Target']}
     for target in ref_targets:
         by_gene[target] = expression_frame(sample_frame, target, ref_sample)
     return pd.DataFrame(by_gene)
-
-def rank_genes(sample_list, ref_genes, ref_sample, with_m=False):
-    # applies the Vandesompele et al. (2002) method to rank reference genes
-    # in order of stability.
-    # doi:10.1186/gb-2002-3-7-research0034
-
-    ref_genes = set(ref_genes)
-
-    table, available_samples = collect_expression(sample_list, ref_genes, ref_sample)
-
-    worst = []
-    worst_m = []
-    while len(ref_genes) - len(worst) > 1:
-        # calculate M for each gene
-        M = []
-        for test_gene in ref_genes:
-            if test_gene in worst: continue
-            Vs = []
-            for ref_gene in ref_genes:
-                if ref_gene == test_gene: continue
-                if ref_gene in worst: continue
-                # compute A_(ref_gene, test_gene)
-                A = [log2(mean(table[ref_gene][test_gene][sample])) for sample in available_samples]
-                Vs.append(std(A))
-            M.append( (sum(Vs) / (len(ref_genes) - len(worst) - 1), test_gene) )
-        worst.append(max(M)[1])
-        worst_m.append(max(M)[0])
-    best = ref_genes - set(worst) # whatever's left over
-    worst.reverse()
-    worst_m.reverse()
-    worst_m = [worst_m[0]] + worst_m
-    if with_m:
-        return list(best) + worst, worst_m
-    else:
-        return list(best) + worst
 
 def rank_target_frame(sample_frame, ref_targets, ref_sample):
     table = collect_expression_frame(sample_frame, ref_targets, ref_sample)
@@ -238,25 +131,6 @@ def rank_target_frame(sample_frame, ref_targets, ref_sample):
     worst_m = [worst_m[0]] + worst_m
     return pd.DataFrame({'Target': list(best) + worst, 'M': worst_m}, columns=['Target', 'M'])
 
-def calculate_all_nfs(sample_list, ranked_genes, ref_sample):
-    # Given a list of sample results and an ordered list of reference genes
-    # (as produced by rank_genes), calculate_all_nfs computes normalization factors
-    # for all samples, using successively larger subsets of the reference gene
-    # list. All computed normalization factors are returned. Use nf_v() to
-    # determine how many reference genes to include in your experimental
-    # samples.
-    # Returns a dictionary nfs, where nfs[n_genes][sample] = nf, for
-    # 2 <= n_genes <= len(ref_genes).
-
-    # d[ref_gene][target][sample] = [rel0, rel1]
-    nfs = {}
-    d = make_sample_dict(sample_list)
-    for i in range(1, len(ranked_genes)+1):
-        nfs[i] = {}
-        for sample in d[ranked_genes[0]]:
-            nfs[i][sample] = gmean([pow(2, -average_cq(d[ref_gene][sample]) + average_cq(d[ref_gene][ref_sample])) for ref_gene in ranked_genes[:i]])
-    return nfs
-
 def calculate_all_nfs_frame(sdf, ranked_genes, ref_sample):
     # Returns a DataFrame, where rows represent samples and columns represent a number of reference genes.
     grouped = sdf.groupby(['Target', 'Sample'])['Cq'].aggregate(average_cq)
@@ -274,14 +148,6 @@ def nfs_from(sample_list, ref_genes, ref_sample):
         if sample == 'NTC': continue
         nfs[sample] = gmean([pow(2, -average_cq(d[ref_gene][sample]) + average_cq(d[ref_gene][ref_sample])) for ref_gene in ref_genes])
     return nfs
-
-def nf_v(nfs):
-    v = {}
-    for i in range(2, max(nfs.keys())+1):
-        if not (i in nfs): break
-        if not (i+1 in nfs): break
-        v[i] = std([ log2(nfs[i][sample]/nfs[i+1][sample]) for sample in nfs[i] ], ddof=1)
-    return v
 
 def nf_v_frame(nfs):
     v = []
--- test_expression.py
+++ test_expression.py
@@ -21,14 +21,7 @@ class TestExpression(unittest.TestCase):
         r = expression_frame(self.sample_frame, 'Ref1', 'Sample 1')
         self.sample_frame['RelExp'] = r
 
-    def test_returns_tuple(self):
-        r = expression(self.sample_frame, 'Ref1', 'Sample 1')
-        self.assertIsInstance(r, TupleType)
-        self.assertEqual(len(r), 3)
-        self.assertIsInstance(r[0], DictType)
-        self.assertIsInstance(r[1], ListType)
-        self.assertIsInstance(r[2], ListType)
-
+"""
     def test_does_not_skip_good_genes_or_samples(self):
         sample_d, ignored_genes, ignored_samples = expression(self.sample_frame, 'Ref1', 'Sample 1')
         self.assertEqual(ignored_genes, [])
@@ -68,6 +61,7 @@ class TestExpression(unittest.TestCase):
         sample_frame = pd.DataFrame(sample_list, columns=['Target', 'Sample', 'Cq'])
         sample_d, ignored_genes, ignored_samples = expression(sample_frame, 'Ref1', 'Sample 1')
         self.assertIn('Exp2', ignored_genes)
+"""
 
 class TestAverage(unittest.TestCase):
     def test_averaging(self):
@@ -90,20 +84,6 @@ class TestRankGenes(unittest.TestCase):
             self.d[key] = pd.DataFrame(self.d[key], columns=['Target', 'Sample', 'Cq'])
         # e.g. d['Fib'] = [['ACTB', 'Fib1', 12.34], ...]
 
-    def test_ranking(self):
-        key = {'Neuroblastoma': ['HPRT1', 'GAPD', 'SDHA', 'UBC', 'HMBS', 'YWHAZ', 'TBP', 'ACTB', 'RPL13A', 'B2M'],
-           'Fib': ['HPRT1', 'GAPD', 'YWHAZ', 'UBC', 'ACTB', 'TBP', 'SDHA', 'RPL13A', 'B2M', 'HMBS'],
-           'Leukocyte': ['UBC', 'YWHAZ', 'B2M', 'GAPD', 'RPL13A', 'TBP', 'SDHA', 'HPRT1', 'HMBS', 'ACTB'],
-           'BM': ['UBC', 'RPL13A', 'YWHAZ', 'HPRT1', 'GAPD', 'SDHA', 'TBP', 'HMBS', 'B2M', 'ACTB'],
-           'Pool': ['SDHA', 'GAPD', 'HMBS', 'HPRT1', 'TBP', 'UBC', 'RPL13A', 'YWHAZ', 'ACTB', 'B2M']}
-
-        for tissue in self.d:
-            ranked = rank_genes(self.d[tissue], self.gene_names, self.d[tissue].ix[0, 'Sample'])
-            # no preference between the two best genes
-            self.assertIn(ranked[0], key[tissue][:2])
-            self.assertIn(ranked[1], key[tissue][:2])
-            self.assertEqual(ranked[2:], key[tissue][2:])
-
     def test_frame_ranking(self):
         key = {'Neuroblastoma': ['HPRT1', 'GAPD', 'SDHA', 'UBC', 'HMBS', 'YWHAZ', 'TBP', 'ACTB', 'RPL13A', 'B2M'],
            'Fib': ['HPRT1', 'GAPD', 'YWHAZ', 'UBC', 'ACTB', 'TBP', 'SDHA', 'RPL13A', 'B2M', 'HMBS'],
@@ -118,27 +98,12 @@ class TestRankGenes(unittest.TestCase):
             self.assertIn(ranked.ix[1, 'Target'], key[tissue][:2])
             self.assertEqual(list(ranked.ix[2:, 'Target']), key[tissue][2:])
 
-    def test_nfs(self):
-        comparison_vs = {'Fib': (3, 0.109409), 'BM': (2, 0.113745), 'Leukocyte': (3, 0.116988),
-                'Pool': (2, 0.202805), 'Neuroblastoma': (4, 0.138337)}
-        for tissue in self.d:
-            ranked = rank_genes(self.d[tissue], self.gene_names, self.d[tissue].ix[0, 'Sample'])
-            nfs = calculate_all_nfs(self.d[tissue], ranked, self.d[tissue].ix[0, 'Sample'])
-            vs = nf_v(nfs)
-            test_v, test_value = comparison_vs[tissue]
-            self.assertAlmostEqual(vs[test_v], test_value, places=5)
-            # for v in sorted(vs.keys()):
-                # if v > 3 and vs[v-1] < 0.15: break
-                # print '%d: %f' % (v, vs[v]),
-                # stdout.flush()
-
     def test_nfs_frame(self):
         comparison_vs = {'Fib': (3, 0.109409), 'BM': (2, 0.113745), 'Leukocyte': (3, 0.116988),
                 'Pool': (2, 0.202805), 'Neuroblastoma': (4, 0.138337)}
         for tissue in self.d:
-            print '\n%s' % tissue
-            ranked = rank_genes(self.d[tissue], self.gene_names, self.d[tissue].ix[0, 'Sample'])
-            nfs = calculate_all_nfs_frame(self.d[tissue], ranked, self.d[tissue].ix[0, 'Sample'])
+            ranked = rank_target_frame(self.d[tissue], self.gene_names, self.d[tissue].ix[0, 'Sample'])
+            nfs = calculate_all_nfs_frame(self.d[tissue], ranked['Target'], self.d[tissue].ix[0, 'Sample'])
             vs = nf_v_frame(nfs)
             test_v, test_value = comparison_vs[tissue]
             self.assertAlmostEqual(vs[test_v], test_value, places=5)
